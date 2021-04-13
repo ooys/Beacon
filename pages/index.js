bedd360867a651b77654ac1fa4a7f62a8ab9706a
Yihong Song <1834395537@qq.com>
2021-04-12 23:21:01 -0400

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Index.module.css";
+
+function Index() {
+    return (
+        <div>
+            <title>Beacon - Home</title>h
+        </div>
+    );
+}
+
+export default Index;
